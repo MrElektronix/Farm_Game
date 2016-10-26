c412a752601a2606a9ec330e69a093c66e6ed2f3
MrElektronix <milovanpelt@gmail.com>
2016-10-26 16:06:04 +0200

    error 2

--- Farm_Game/Assets/Scripts/Milo Scripts/Tile_Generation.cs
+++ Farm_Game/Assets/Scripts/Milo Scripts/Tile_Generation.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class test2 : MonoBehaviour {
+public class Tile_Generation : MonoBehaviour {
+    public GameObject Tile;
 
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
