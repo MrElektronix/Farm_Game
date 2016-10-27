19d43eaf88b0bcdbcd48e1fdbabf7e3103f69811
MrElektronix <milovanpelt@gmail.com>
2016-10-27 17:08:06 +0200

    stuff

---
+++ Farm_Game/Assets/Scripts/Milo Scripts/SeedPlacement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeedPlacement : MonoBehaviour {
+    public GameObject seed;
+    private Transform trans;
+
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
