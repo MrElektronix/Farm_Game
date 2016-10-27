be2f2f7a0dda45803c28d9a356114fdd94697e4f
SkytheQ <lieskuh11@hotmail.com>
2016-10-28 00:02:52 +0200

    Hoverstuff
    doesn't work yet.. for some reason

---
+++ Farm_Game/Assets/Scripts/Lisa Scripts/gameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameManager : Singleton<gameManager>
+{
+
+    public plantBTN clickedBTN { get; set; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleEscape();
+    }
+
+    public void pickPlant(plantBTN plantBtn)
+    {
+        this.clickedBTN = plantBtn;
+        Hover.Instance.Activate(plantBtn.Sprite);
+    }
+
+    public void ButPlant()
+    {
+        Hover.Instance.Deactivate();
+    }
+
+    private void HandleEscape()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Hover.Instance.Deactivate();
+        }
+    }
+}
---
+++ Farm_Game/Assets/Scripts/Lisa Scripts/plantBTN.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class plantBTN : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject plantPrefab;
+
+    [SerializeField]
+    private Sprite sprite;
+
+    public GameObject PlantPrefab
+    {
+        get
+        {
+            return plantPrefab;
+        }
+
+    }
+
+    public Sprite Sprite
+    {
+        get
+        {
+            return sprite;
+        }
+
+    }
+}
