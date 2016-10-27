c66334a354957481621027703fcbed6fa9eee004
MrElektronix <milovanpelt@gmail.com>
2016-10-28 01:32:51 +0200

    Farm_Game 28 oktober 01:32
    - Working on Timers for the plants

---
+++ Farm_Game/Assets/Scripts/Milo Scripts/TimersUI_Plants.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimersUI_Plants : MonoBehaviour {
+
+    [SerializeField]
+    private Text _TomatoText;
+
+    [SerializeField]
+    private Text _CarrotText;
+
+    [SerializeField]
+    private Text _StrawberryText;
+
+    [SerializeField]
+    private Text _CornText;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SetTimerTextPlants(string text, float time) {
+        //GameObject Player = GameObject.Find("Player");
+        //Timers_Plants timerplants = Player.GetComponent<Timers_Plants>();
+
+        switch (text)
+        {
+            case "Tomato":
+                _TomatoText.text = "Tomato: " + time.ToString();
+                break;
+            case "Carrot":
+                _CarrotText.text = "Carrot: " + time.ToString();
+                break;
+            case "Strawberry":
+                _StrawberryText.text = "Strawberry: " + time.ToString();
+                break;
+            case "Corn":
+                _CornText.text = "Corn: " + time.ToString();
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Farm_Game/Assets/Scripts/Milo Scripts/Timers_Plants.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timers_Plants : MonoBehaviour {
+
+    public float TomatoTime = 5;
+    public float CarrotTime = 2;
+    public float StrawberryTime = 10;
+    public float CornTime = 3;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        TomatoTime -= Time.deltaTime;
+        TomatoTime = Mathf.Round(TomatoTime * 100f) / 100f;
+        Timer();
+    }
+
+    void Timer()
+    {
+        if (TomatoTime <= 0f) { TomatoTime = 0f; }
+        if (CarrotTime <= 0f) { CarrotTime = 0f; }
+        if(StrawberryTime <= 0f) { StrawberryTime = 0f; }
+        if (CornTime <= 0f) { CornTime = 0f;}
+
+        GameObject Cube = GameObject.Find("Cube");
+        TimersUI_Plants timeruiplants = Cube.GetComponent<TimersUI_Plants>();
+
+        timeruiplants.SetTimerTextPlants("Tomato", TomatoTime);
+        timeruiplants.SetTimerTextPlants("Carrot", CarrotTime);
+        timeruiplants.SetTimerTextPlants("Strawberry", StrawberryTime);
+        timeruiplants.SetTimerTextPlants("Corn", CornTime);
+
+    }
+
+}
